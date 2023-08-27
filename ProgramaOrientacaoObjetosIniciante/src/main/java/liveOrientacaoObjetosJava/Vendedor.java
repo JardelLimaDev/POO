8660fafadba3f4cc27377d983eceacafc63d70e7
jarde <jarde@LAPTOP-R1K52MQF>
2023-08-27 19:34:53 -0300

    Classe Vendedor

---
+++ ProgramaOrientacaoObjetosIniciante/src/main/java/liveOrientacaoObjetosJava/Vendedor.java
@@ -0,0 +1,27 @@
+package liveOrientacaoObjetosJava;
+
+public class Vendedor {
+	private String nome;
+	private String cpf;
+	private String matricula;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	} 
+	
+	
+}
